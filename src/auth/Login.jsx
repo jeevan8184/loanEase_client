2a5f2b8d337b3f3cc99c3ce7ef67346092866d3a
jeevan8184 <jeevanjatavath7@gmail.com>
2024-10-21 20:00:42 +0530

    first commit

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -16,7 +16,6 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { TOKEN, USER } from "../constants";
-import { jwtDecode } from "jwt-decode";
 
 
 const Login = () => {
